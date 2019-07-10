f52987732db7fef04738fef5feb7c26cea89e994
joeldiniz20 <49651419+joeldiniz20@users.noreply.github.com>
2019-07-10 13:37:55 +0100

    Update AccountController.cs

--- MvcMusicStore/Controllers/AccountController.cs
+++ MvcMusicStore/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ namespace Mvc3ToolsUpdateWeb_Default.Controllers
 {
     public class AccountController : Controller
     {
-        //add some code to trigger a CI build
+        //add some code to trigger a CI build new
         private void MigrateShoppingCart(string UserName)
         {
             // Associate shopping cart items with logged-in user
